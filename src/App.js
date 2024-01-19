262b893fd4b1a6892f90b7539ecf52217427e93e
henny <kimhyein1105@gmail.com>
2024-01-19 23:44:39 +0900

    리액트 초기 셋팅

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import Swiper from './components/Swiper';
+
+function App() {
+	return (
+		<div>
+			<h1>Welcome back!</h1>
+			<Swiper />
+		</div>
+	);
+}
+
+export default App;
---
+++ src/components/Swiper.jsx
@@ -0,0 +1,5 @@
+import React, { useEffect, useRef } from 'react';
+import Swiper from 'swiper';
+import 'swiper/css';
+
+export default function SwiperComponent() {}
