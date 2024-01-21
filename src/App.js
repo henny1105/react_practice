4c9083257f8b39faa5348e35d5d5e73c1c201a73
henny <kimhyein1105@gmail.com>
2024-01-22 00:04:20 +0900

    a링크로 슬라이드 예제 불러오기

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Main from './components/Main';
+import SwiperContent from './components/SwiperContent';
 
 function App() {
-	return <Main />;
+	return (
+		<Router>
+			<Routes>
+				<Route path='/' element={<Main />} />
+				<Route path='/swiper-content' element={<SwiperContent />} />
+			</Routes>
+		</Router>
+	);
 }
 
 export default App;
